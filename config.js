d87de5bc2e5a22a6e28cce3b0ca378085afd32ef
Maxim <74974283+maximka76667@users.noreply.github.com>
2022-04-08 20:36:45 +0200

    setup

---
+++ config.js
@@ -0,0 +1,21 @@
+const { LOGIN, PASSWORD } = process.env;
+const config = {};
+
+config.ALLOWED_CORS = [
+  'https://max76667.movies.nomoredomains.club',
+  'http://max76667.movies.nomoredomains.club',
+  'max76667.movies.nomoredomains.club',
+  'localhost:3000',
+  'http://localhost:3000',
+  'localhost:3001',
+  'http://localhost:3001',
+  'localhost:5000',
+  'http://localhost:5000',
+  'https://maximka76667.github.io',
+  'maximka76667.github.io',
+];
+config.DEFAULT_ALLOWED_METHODS = 'GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS';
+config.JWT_SECRET_DEV = 'jwt-secret';
+config.DB_URL = `mongodb+srv://${LOGIN}:${PASSWORD}@cluster0.vdazw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+module.exports = config;
