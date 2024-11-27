168de9e940a748c8101f2eaeb1859a6cca7c4b0a
Maxim <74974283+maximka76667@users.noreply.github.com>
2024-11-27 20:15:02 +0100

    refact
    delete old hostings config files

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
-const { LOGIN, PASSWORD } = process.env;
+const { LOGIN, PASS } = process.env;
 const config = {};
 
 config.ALLOWED_CORS = [
-  'localhost:3000',
-  'http://localhost:3000',
-  'localhost:3001',
-  'http://localhost:3001',
-  'localhost:5000',
-  'http://localhost:5000',
-  'https://maximka76667.github.io',
-  'maximka76667.github.io',
+  "localhost:3000",
+  "http://localhost:3000",
+  "localhost:3001",
+  "http://localhost:3001",
+  "localhost:5000",
+  "http://localhost:5000",
+  "https://maximka76667.github.io",
+  "maximka76667.github.io",
 ];
-config.DEFAULT_ALLOWED_METHODS = 'GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS';
-config.JWT_SECRET_DEV = 'jwt-secret';
-config.DB_URL = `mongodb+srv://${LOGIN}:${PASSWORD}@cluster0.vdazw.mongodb.net/whatDidIDo?retryWrites=true&w=majority`;
+config.DEFAULT_ALLOWED_METHODS = "GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS";
+config.JWT_SECRET_DEV = "jwt-secret";
+config.DB_URL = `mongodb+srv://${LOGIN}:${PASS}@what-did-i-do-db.8efg8.mongodb.net/?retryWrites=true&w=majority&appName=What-Did-I-Do-DB`;
 
 module.exports = config;
